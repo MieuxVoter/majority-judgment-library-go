9a01018188987681690e431207a0557f0c1b126d
domi41 <dominique.merle.41@gmail.com>
2021-07-02 23:37:42 +0200

    test: improve code coverage to 96%

--- majorityjudgment_test.go
+++ majorityjudgment_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestReadmeDemo(t *testing.T) {
-
 	poll := &PollTally{
 		AmountOfJudges: 10,
 		Proposals: []*ProposalTally{
@@ -28,4 +27,29 @@ func TestReadmeDemo(t *testing.T) {
 	assert.Equal(t, 2, result.Proposals[2].Rank, "Rank of proposal C")
 	assert.Equal(t, 3, result.Proposals[3].Rank, "Rank of proposal D")
 	assert.Equal(t, 4, result.Proposals[4].Rank, "Rank of proposal E")
+}
+
+func TestNoProposals(t *testing.T) {
+	poll := &PollTally{
+		AmountOfJudges: 10,
+		Proposals:      []*ProposalTally{},
+	}
+	deliberator := &MajorityJudgment{}
+	result, err := deliberator.Deliberate(poll)
+	assert.NoError(t, err, "Deliberation should succeed")
+	assert.Len(t, result.Proposals, len(poll.Proposals), "There should be as many results as there are tallies.")
+}
+
+func TestMishapedTally(t *testing.T) {
+	poll := &PollTally{
+		AmountOfJudges: 10,
+		Proposals: []*ProposalTally{
+			{Tally: []uint64{2, 2, 2, 2, 2}},
+			{Tally: []uint64{2, 2, 2, 2}},
+		},
+	}
+	deliberator := &MajorityJudgment{}
+	result, err := deliberator.Deliberate(poll)
+	assert.Error(t, err, "Deliberation should fail")
+	assert.Nil(t, result, "Deliberation result should be nil")
 }
